f24ad7f4e223ac4fc2495da03e32ce4d09cac138
ValerioMeschi <valerio.meschi@gmail.com>
2023-08-11 14:28:46 +0200

    Update zeevector.js
    Some documentation

--- build/zeevector.js
+++ build/zeevector.js
@@ -1,31 +1,45 @@
 //A Quick and dirty vector class for creative coding.
 
 class ZVector {
-    //Vector class, takes in 2 or 3 dimensional floats.
-    constructor(_x = 0, _y = 0, _z = 0) {
-        this.x = _x;
-        this.y = _y;
-        this.z = _z;
+    /**
+     * Vector class, takes in 2 or 3 dimensional floats.
+     * @param {number} x x value of vector [defaults to 0]
+     * @param {number} y y value of vector [defaults to 0]
+     * @param {number} z z value of vector [defaults to 0]
+     */
+    constructor(x = 0, y = 0, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     ///////////////////////////////////////////////////////
-    //UTILITY METHODS
+    //  UTILITY METHODS
     //
+    /**
+     * @returns a clone of the vector.
+    **/
     clone() {
-        //clones the vector into a new instance.
         let newv = new ZVector(this.x, this.y, this.z);
         return newv;
     }
+    /**
+     * Utility
+     * @returns the vector as a string.
+     */
     toString() {
-        //returns the vector in string form.
         if (this.z == 0) {
             return "[x = " + this.x + ", y = " + this.y + "]";
         } else {
             return "[x = " + this.x + ", y = " + this.y + ", z = " + this.z + "]";
         }
     }
+    /**
+     * Utility
+     * @returns the vector as an object. 
+     */
     toObject() {
-        //returns the vector in object form.
+        //
         if (this.z == 0) {
             return {
                 x: this.x,
@@ -39,8 +53,11 @@ class ZVector {
             };
         }
     }
+    /**
+     * Utility
+     * @returns the vector as an array
+     */
     toArray() {
-        //returns the vector as an array.
         if (this.z == 0) {
             return [this.x, this.y];
         } else {
@@ -51,8 +68,14 @@ class ZVector {
     ///////////////////////////////////////////////////////
     //MANIPULATION METHODS
     //
+    /**
+     * Set the values of the vector's components.
+     * @param {number} x new X value
+     * @param {number} y new Y value
+     * @param {number} z new Z value [defaults to 0]
+     */
     set(x, y, z = 0) {
-        //set the value of the vector's components.
+        //
         if (typeof x == "number" && typeof y == "number") {
             this.x = x;
             this.y = y;
@@ -60,9 +83,13 @@ class ZVector {
         } else {
             console.error("vector components must be numbers")
         }
+        return this;
     }
+    /**
+     * Adds the vector passed as a parameter to the one calling the function.
+     * @param {ZVector} v vector to be added to this vector.
+     */
     add(v) {
-        //Adds the vector passed as a parameter to the one calling the function.
         if (v instanceof ZVector) {
             this.x = this.x + v.x;
             this.y = this.y + v.y;
@@ -70,10 +97,14 @@ class ZVector {
         } else {
             console.error("the added vector must be a ZVector too !")
         }
+        return this;
 
     }
+    /**
+     * Substracts the vector passed as a parameter to the one calling the function.
+     * @param {ZVector} v vector to be subtracted to this vector.
+     */
     sub(v) {
-        //Substracts the vector passed as a parameter to the one calling the function.
         if (v instanceof ZVector) {
             this.x = this.x - v.x;
             this.y = this.y - v.y;
@@ -81,9 +112,13 @@ class ZVector {
         } else {
             console.error("the added vector must be a ZVector too !")
         }
+        return this;
     }
+    /**
+     * Multiplies the vector components by a scalar.
+     * @param {number} s Scalar
+     */
     mult(s) {
-        //Multiplies the vector components by a scalar.
         if (typeof s == "number") {
             this.x = this.x * s;
             this.y = this.y * s;
@@ -91,10 +126,13 @@ class ZVector {
         } else {
             console.error("Parameter must be a number");
         }
-
+        return this;
     }
+    /**
+     * Divides the vector components by a scalar.
+     * @param {number} s Scalar
+     */
     div(s) {
-        //Divides the vector components by a scalar.
         if (typeof s == "number") {
             this.x = this.x / s;
             this.y = this.y / s;
@@ -102,12 +140,28 @@ class ZVector {
         } else {
             console.error("Parameter must be a number");
         }
+        return this;
     }
-    rotate() {
-
+    /**
+     * Rotates the vector around an axis.
+     * @param {number} ang Angle of rotation in radians
+     * @param {*} ax Axis of rotation as a Zvector [Defaults to z unit vector]
+     * @returns The rotated vector
+     */
+    rotate(ang, ax = new ZVector(0,0,1)) {
+        ax.normalize();
+        const sinAng = Math.sin(ang);
+        const cosAng = Math.cos(ang);
+        const dotProduct = this.dot(ax);
+        const crossProduct = this.cross(ax);
+        this.mult(cosAng).add(crossProduct.mult(sinAng)).add(ax.mult(dotProduct * (1 - cosAng)));
+        return this;
     }
+    /**
+     * Sets the magnitude of the vector.
+     * @param {number} s Scalar 
+     */
     setMag(s) {
-        //sets the magnitude of the vector
         if (typeof s == "number") {
             let m = this.getMag();
             let r = m / s;
@@ -115,6 +169,7 @@ class ZVector {
         } else {
             console.error("Parameter must be a number");
         }
+        return this
     }
     random() {
         //randomizes the vector into a 
@@ -122,8 +177,10 @@ class ZVector {
         this.y = 1 - Math.random() * 2;
         this.normalize();
     }
+    /**
+     * normalizes the vector to a magnitude of 1;
+     */
     normalize() {
-        //normalizes the vector to a magnitude of 1 aka a "unit vector".
         let m = this.getMag()
         this.div(m);
     }
@@ -137,10 +194,10 @@ class ZVector {
     }
     cross(v) {
         //returns the cross product of two vectors as a ZVector.
-        let nx = this.y * v.z - this.z * v.y;
-        let ny = this.z * v.x - this.x * v.z;
-        let nz = this.x * v.y - this.y * v.x;
-        return new ZVector(nx, ny, nz);
+        this.x = this.y * v.z - this.z * v.y;
+        this.y = this.z * v.x - this.x * v.z;
+        this.z = this.x * v.y - this.y * v.x;
+        return this
     }
     getMag() {
         //retuns the magnitude of the vector as a scalar.
@@ -154,7 +211,5 @@ class ZVector {
     }
 }
 
-let v = new ZVector();
-v.random();
+let v = new ZVector(0,1);
 console.log(v.toString());
-console.log(v.getMag());
